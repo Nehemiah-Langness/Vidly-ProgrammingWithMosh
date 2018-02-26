76774dd0c9df90e4ad47f6416832aa1f5cf2faef
Nehemiah Langness <Nehemiah.Langness@hotmail.com>
2018-02-26 15:36:20 -0600

    Node Module System - OS Module

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
-const path = require('path');
+const os = require('os');
 
-var pathObj = path.parse(__filename);
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
 
-console.log(pathObj);
+console.log(`Total Memory ${totalMemory}`);
+console.log(`Free Memory ${freeMemory}`);
