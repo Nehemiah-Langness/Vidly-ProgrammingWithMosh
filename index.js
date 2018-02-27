f2c3a059ae7687caa1666f6d2d09fb2a737d6dd3
Nehemiah Langness <Nehemiah.Langness@hotmail.com>
2018-02-27 15:41:10 -0600

    Building RESTful API's Using Express - Handling HTTP Delete Requests

--- index.js
+++ index.js
@@ -28,13 +28,8 @@ app.get('/api/courses', (req, res) => {
 app.post('/api/courses', (req, res) => {
     const { error } = validateCourse(req.body);
 
-    if (error) {
-        res.status(400).send(error.details.map((e) => e.message).reduce((current, next) => {
-            if (!current) return next;
-            return `${current},\n${next}`;
-        }));
-        return;
-    }
+    if (error)
+        return res.status(400).send(error.details.map((e) => e.message).reduce((current, next) => { return !current ? next : `${current},\n${next}`; }));
 
     const course = {
         id: courses.length + 1,
@@ -46,24 +41,28 @@ app.post('/api/courses', (req, res) => {
 
 app.put('/api/courses/:id', (req, res) => {
     const course = courses.find((c) => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('The course with the given ID does not exist');
+    if (!course) return res.status(404).send('The course with the given ID does not exist');
 
     const { error } = validateCourse(req.body);
-    if (error) {
-        res.status(400).send(error.details.map((e) => e.message).reduce((current, next) => {
-            if (!current) return next;
-            return `${current},\n${next}`;
-        }));
-        return;
-    }
+    if (error)
+        return res.status(400).send(error.details.map((e) => e.message).reduce((current, next) => { return !current ? next : `${current},\n${next}`; }));
 
     course.name = req.body.name;
     res.send(course);
 });
 
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find((c) => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('The course with the given ID does not exist');
+
+    courses.splice(courses.indexOf(course), 1);
+    res.send(course);
+});
+
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find((c) => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('The course with the given ID does not exist');
+    if (!course) return res.status(404).send('The course with the given ID does not exist');
+
     res.send(course);
 });
 
