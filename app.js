34f89478c8a681b7e0e9a860ce4eecb0d4f4a083
Nehemiah Langness <Nehemiah.Langness@hotmail.com>
2018-02-27 09:46:52 -0600

    Node Module System - Http Module

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 
-const EventEmitter = require('events');
+const http = require('http');
 
-const Logger = require('./logger');
-const logger = new Logger();
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('Hello world');
+        res.end();
+    }
 
-// Register a listener
-logger.on('messageLogged', (arg) => {
-    console.log('Listener called', arg);
+    if (req.url === '/api/courses') {
+        res.write(JSON.stringify([1, 2, 3]));
+        res.end();
+    }
 });
+server.listen(3000);
 
-logger.log('Message');
+console.log('Listening on port 3000...');
