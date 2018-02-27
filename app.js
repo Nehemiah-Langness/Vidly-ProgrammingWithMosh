2a1d78ba3ff6ae7e7edd56190026dfd3cfadad37
Nehemiah Langness <Nehemiah.Langness@hotmail.com>
2018-02-27 09:38:30 -0600

    Node Module System - Extending Event Emitter

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 
 const EventEmitter = require('events');
-const emitter = new EventEmitter();
 
-emitter.on('messageLogged', (arg) => {
+const Logger = require('./logger');
+const logger = new Logger();
+
+// Register a listener
+logger.on('messageLogged', (arg) => {
     console.log('Listener called', arg);
 });
 
-emitter.emit('messageLogged', { id: 1, url: 'http://' });
-
+logger.log('Message');
--- logger.js
+++ logger.js
@@ -1,9 +1,17 @@
 
+const EventEmitter = require('events');
+
 var url = 'http://mylogger.io/log'
 
-function log(message) {
-    // Send an HTTP request
-    console.log(message);
+class Logger extends EventEmitter {
+
+    log(message) {
+        // Send an HTTP request
+        console.log(message);
+
+        // Raise an event
+        this.emit('messageLogged', { message: message });
+    }
 }
 
-module.exports = log;
+module.exports = Logger;
