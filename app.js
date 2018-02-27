8911a009c0bcba2e5cb0d8a82253cca57fa03dab
Nehemiah Langness <Nehemiah.Langness@hotmail.com>
2018-02-27 09:30:39 -0600

    Node Module System - Events Arguments

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
-emitter.on('messageLogged', function () {
-    console.log('Listener called');
+emitter.on('messageLogged', (arg) => {
+    console.log('Listener called', arg);
 });
 
-emitter.emit('messageLogged');
+emitter.emit('messageLogged', { id: 1, url: 'http://' });
 
