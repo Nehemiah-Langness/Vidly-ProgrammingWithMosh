341bc888e3b5bef13d4a83f201589d66fe66988b
Nehemiah Langness <Nehemiah.Langness@hotmail.com>
2018-02-27 13:12:44 -0600

    Building RESTful API's Usnig Express - Route Parameters

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.get('/api/courses', (req, res) => {
 });
 
 
+app.get('/api/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const query = req.query;
+    res.send(query);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`)
