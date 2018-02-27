2cea7365d739b7414e63e7f19ca256cd549dc21e
Nehemiah Langness <Nehemiah.Langness@hotmail.com>
2018-02-27 14:08:52 -0600

    Building RESTful API's Using Express - Handling HTTP POST requests

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+const courses = [{
+        id: 1,
+        name: 'Course 1'
+    },
+    {
+        id: 2,
+        name: 'Course 2'
+    },
+    {
+        id: 3,
+        name: 'Course 3'
+    }
+]
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3, 4, 5]);
+    res.send(courses);
 });
 
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course);
+    res.send(course);
+});
 
 app.get('/api/courses/:id', (req, res) => {
-    const id = req.params.id;
-    const query = req.query;
-    res.send(query);
+    const course = courses.find((c) => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the given ID does not exist');
+    res.send(course);
 });
 
 const port = process.env.PORT || 3000;
