434e17c87adf9516b0c2c0464af7528c1184d75e
Nehemiah Langness <Nehemiah.Langness@hotmail.com>
2018-03-01 16:02:31 -0600

    Began Mongoose integration

--- vidly.js
+++ vidly.js
@@ -1,4 +1,43 @@
 const debug = require('debug')('app:startup');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground')
+    .then(() => debug('Connected to database'))
+    .catch(error => console.error('Unable to connect to database', error));
+
+// Available Schema types:
+// String, Number, Date, Buffer, Boolean, ObjectID, Array
+const Genre = mongoose.model('Genre', new mongoose.Schema({
+    name: String,
+    tags: [String],
+    createDate: { type: Date, default: Date.now }
+}));
+
+// async function createGenre() {
+//     const genre = new Genre({
+//         name: 'Comedy',
+//         tags: ['Funny', 'HaHa']
+//     });
+
+//     var result = await genre.save()
+//     console.log(result);
+// }
+// createGenre();
+
+async function getGenres() {
+    genres = await Genre
+        .find({
+            'tags.length': {
+                $eq: 1
+            }
+        })
+        .limit(10)
+        .sort({ name: 1 })
+        .select({ name: 1, tags: 1 });
+
+    console.log(genres);
+}
+getGenres();
 
 const express = require('express');
 const app = express();
