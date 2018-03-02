54fc960d13ba571de075c461cbb5a93b5a802bd6
Nehemiah Langness <Nehemiah.Langness@hotmail.com>
2018-03-02 13:17:19 -0600

    Fixed undefined bug; Refactoring;

--- routes/common.js
+++ routes/common.js
@@ -10,79 +10,10 @@ function serverError(res, error) {
     res.status(500).send(getErrorMessage(error));
 }
 
-function getErrorMessage(error) {
-    return error.details
-        .map((e) => e.message)
-        .reduce((current, next) => !current ? next : `${current},\n${next}`)
-}
-
-function addCrudPaths(router, repo) {
-    const { validate, repository } = repo;
-
-    // Get-all
-    router.get('/', async (req, res) => {
-        try {
-            const entities = await repository.get();
-            res.send(entities);
-        } catch (error) {
-            common.send(res).serverError(error);
-        }
-    });
-
-    // Get
-    router.get('/:id', async (req, res) => {
-        try {
-            const entity = await repository.get(req.params.id);
-            if (!entity) return common.send(res).notFound();
-
-            res.send(entity);
-        } catch (error) {
-            common.send(res).serverError(error);
-        }
-    });
-
-    // Add
-    router.post('/', async (req, res) => {
-        try {
-            var toAdd = req.body;
-            const error = validate(toAdd);
-            if (error) return common.send(res).badRequest(error);
-
-            const entity = await repository.add(toAdd);
-            res.send(entity);
-
-        } catch (error) {
-            common.send(res).serverError(error);
-        }
-    });
-
-    // Update
-    router.put('/:id', async (req, res) => {
-        try {
-            const toUpdate = req.body;
-            const error = validate(toUpdate);
-            if (error) return common.send(res).badRequest(error);
-
-            const entity = await repository.update(req.params.id, toUpdate)
-            if (!entity) return common.send(res).notFound();
-
-            res.send(entity);
-        } catch (error) {
-            common.send(res).serverError(error);
-        }
-    });
-
-    // Delete
-    router.delete('/:id', async (req, res) => {
-        try {
-            const genre = await repository.remove(req.params.id);
-            if (!genre) return common.send(res).notFound();
-
-            res.send(genre);
-        } catch (error) {
-            common.send(res).serverError(error);
-        }
-    });
-}
-
-module.exports.addBasicCrud = addCrudPaths;
+module.exports.send = function(response) {
+    return {
+        notFound: () => notFound(response),
+        badRequest: (error) => badRequest(response, error),
+        serverError: (error) => serverError(response, error)
+    }
+};
--- routes/crud.js
+++ routes/crud.js
@@ -1,5 +1,76 @@
+const common = require('./common');
+
+function addCrudPaths(router, repo) {
+    const { validate, repository } = repo;
+
+    // Get-all
+    router.get('/', async (req, res) => {
+        try {
+            const entities = await repository.get();
+            res.send(entities);
+        } catch (error) {
+            common.send(res).serverError(error);
+        }
+    });
+
+    // Get
+    router.get('/:id', async (req, res) => {
+        try {
+            const entity = await repository.get(req.params.id);
+            if (!entity) return common.send(res).notFound();
+
+            res.send(entity);
+        } catch (error) {
+            common.send(res).serverError(error);
+        }
+    });
+
+    // Add
+    router.post('/', async (req, res) => {
+        try {
+            var toAdd = req.body;
+            const error = validate(toAdd);
+            if (error) return common.send(res).badRequest(error);
+
+            const entity = await repository.add(toAdd);
+            res.send(entity);
+
+        } catch (error) {
+            common.send(res).serverError(error);
+        }
+    });
+
+    // Update
+    router.put('/:id', async (req, res) => {
+        try {
+            const toUpdate = req.body;
+            const error = validate(toUpdate);
+            if (error) return common.send(res).badRequest(error);
+
+            const entity = await repository.update(req.params.id, toUpdate)
+            if (!entity) return common.send(res).notFound();
+
+            res.send(entity);
+        } catch (error) {
+            common.send(res).serverError(error);
+        }
+    });
+
+    // Delete
+    router.delete('/:id', async (req, res) => {
+        try {
+            const genre = await repository.remove(req.params.id);
+            if (!genre) return common.send(res).notFound();
+
+            res.send(genre);
+        } catch (error) {
+            common.send(res).serverError(error);
+        }
+    });
+}
+
 module.exports = function(model) {
     const router = require('express').Router();
-    require('./common').addBasicCrud(router, model);
+    addCrudPaths(router, model);
     return router;
 }
